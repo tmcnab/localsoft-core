16d8ccb143274be8111e043a8e8b1ac3d3767196
Tristan McNab <tristan.j.mcnab@gmail.com>
2018-11-14 22:50:48 -0500

    fe dependencies

---
+++ client/src/Application.js
@@ -0,0 +1,14 @@
+import {BrowserRouter} from 'react-router-dom'
+import React, { Component } from 'react';
+
+
+export default class App extends Component {
+
+    render = () =>
+        <BrowserRouter>
+            <>
+                Hello world
+            </>
+        </BrowserRouter>
+
+}
