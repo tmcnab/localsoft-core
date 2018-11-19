fbe2de340fa301a46498959243248401f70a1570
Tristan McNab <tristan.j.mcnab@gmail.com>
2018-11-18 19:26:17 -0500

    pouchdb

--- server/config.mjs
+++ server/config.mjs
@@ -1,13 +1,18 @@
 import path from 'path'
 import uuid from 'uuid/v4'
+import PouchDB from 'pouchdb-node'
 
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname)
 const PRODUCTION = process.env.NODE_ENV === 'production'
 
 
+export const db = new PouchDB('../localsoft.db')
+
+
 export default ({
     BUILD_DIR: path.join(__dirname, '..', '.build'),
+    DATA_DIR: path.join(__dirname, '..', '.data'),
     PRODUCTION,
     PORT: 3001,
     SECRET: PRODUCTION ? uuid()  : 'secret'
