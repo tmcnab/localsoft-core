0a7f940db25b4397900d68f00b5b786a1535ac7f
Tristan McNab <tristan.j.mcnab@gmail.com>
2018-12-01 20:19:48 -0500

    refactoring route code into route dir

--- server/index.mjs
+++ server/index.mjs
@@ -1,16 +1,14 @@
 import compression from 'compression'
 import cookieSession from 'cookie-session'
-import config, {db} from './config'
+import config from './config'
+import download from './routes/download'
 import express from 'express'
 import expressGraphQL from 'express-graphql'
 import helmet from 'helmet'
 import http from 'http'
-import multer from 'multer'
 import patch from './routes/patch'
-import path from 'path'
 import schema from './schema'
-import uuid from 'uuid/v4'
-
+import upload from './routes/upload'
 
 
 // Create app and configure settings.
@@ -42,61 +40,10 @@ app.use('/graphql', expressGraphQL({
     schema,
 }))
 
-
+// Register custom route handlers.
 app.patch('/:resource/', patch)
-
-
-// Configure uploads.
-const onlyStaffAndAdmins = (req, res, next) => {
-    const {role} = req.session
-    if (!['STAFF', 'ADMINISTRATOR'].includes(role)) {
-        return res.status(401).json({
-            error: 'Unauthorized'
-        })
-    }
-    next()
-}
-
-const upload = multer({
-    storage: multer.diskStorage({
-        destination: (req, file, cb) => cb(null, config.DATA_DIR),
-        filename: (req, file, cb) => cb(null, uuid())
-    })
-})
-
-app.post('/upload', onlyStaffAndAdmins, upload.any(), (request, response) => {
-    const access = request.session.role === 'STAFF' ? 'STAFF' : 'ADMINISTRATOR'
-    request.files.forEach(file => {
-        db.get('files').push({
-            access,
-            description: "",
-            identifier: file.filename,
-            mimeType: file.mimetype,
-            name: file.originalname,
-            size: file.size,
-            tags: [],
-            uploaded: new Date().toISOString(),
-        }).write()
-    })
-    response.status(201).json({status: 'success'})
-})
-
-// Ability for users to download files.
-app.get('/files/:identifier', (request, response) => {
-    const {identifier} = request.params
-    const record = db.get('files').find({identifier}).value()
-
-    // No record? 404.
-    if (!record) {
-        return response.sendStatus(404)
-    }
-
-    // Where file is located on disk.
-    const file = path.join(config.DATA_DIR, identifier)
-
-    // TODO: restrict by role on record/request.session.role (404 to prevent leakage)
-    response.download(file, file.name)
-})
+app.get('/files/:identifier', download)
+app.post('/upload', upload)
 
 
 // In production we serve the build directory from static
---
+++ server/routes/upload.mjs
@@ -0,0 +1,46 @@
+import config, {db} from '../config'
+import multer from 'multer'
+import uuid from 'uuid/v4'
+
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => cb(null, config.DATA_DIR),
+        filename: (req, file, cb) => cb(null, uuid())
+    })
+}).any()
+
+
+export default (request, response, next) => {
+    const {role} = request.session
+
+    // Only staff and admins are authorized to upload files.
+    if (!['STAFF', 'ADMINISTRATOR'].includes(role)) {
+        const error = new Error('Unauthorized')
+        error.statusCode = 401
+        return next(error)
+    }
+
+    upload(request, response, (err) => {
+        if (err instanceof multer.MulterError) {
+            // TODO: A Multer error occurred when uploading.
+        } else if (err) {
+            // TODO: An unknown error occurred when uploading.
+        } else {
+            const access = role === 'STAFF' ? 'STAFF' : 'ADMINISTRATOR'
+            request.files.forEach(file => {
+                db.get('files').push({
+                    access,
+                    description: "",
+                    identifier: file.filename,
+                    mimeType: file.mimetype,
+                    name: file.originalname,
+                    size: file.size,
+                    tags: [],
+                    uploaded: new Date().toISOString(),
+                }).write()
+            })
+            response.status(201).json({status: 'success'})
+        }
+    })
+}
