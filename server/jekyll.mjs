44435f117b1219e53cb508011ddd332bc7181ad3
Tristan McNab <tristan.j.mcnab@gmail.com>
2018-12-12 18:37:24 -0500

    minor adjustments

--- server/jekyll.mjs
+++ server/jekyll.mjs
@@ -1,7 +1,13 @@
 import {exec} from 'child_process'
 
-export default async () => {
-    return await new Promise((resolve, reject) => {
+/**
+ *  Regenerates the jekyll
+ *
+ *  @param  {String} zipFileName the ID of the file containing a jekyll theme to install.
+ */
+export default (themeFile = null) => {
+    // TODO: check for zipFileName
+    new Promise((resolve, reject) => {
         exec('cd ../.jekyll && jekyll build', error =>
             error ? reject(error) : resolve()
         )
