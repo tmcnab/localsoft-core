d106ec889d04d4190258cf32486aea738aab1a99
Tristan McNab <tristan.j.mcnab@gmail.com>
2020-11-25 17:15:21 -0500

    reset

--- gql.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const executeGraphQL = async (url, query, variables) =>
-	(await axios.post(url, { query, variables })).data.data
-
-export default async (query, variables) => {
-	const url = '/api/graphql'
-	return await executeGraphQL(url, query, variables)
-}
